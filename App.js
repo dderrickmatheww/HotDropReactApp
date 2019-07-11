08249f0d9e4e491bcfd9dac882d4994ef68675a1
Robert Butler <robwbutler@gmail.com>
2019-07-11 19:23:38 -0400

    working android test stuff

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, KeyboardAvoidingView } from 'react-native';
 import Header from './components/header/header';
+import Card from './components/card/card';
+
 
 export default function App() {
+
+  //these state values were hardcoded for testing, delete and replace with fetch requests, as well as a property for the thumbnail/article image
+  state = {
+    articles: [
+      {
+        title: "Title 1",
+        subheader: "Subhead 1",
+        body: "This is an article or game info card. Blah blah blah" 
+      },
+      {
+        title: "Title 2",
+        subheader: "Subhead 2",
+        body: "This is an article or game info card. Blah blah blah" 
+      },
+      {
+        title: "Title 3",
+        subheader: "Subhead 3",
+        body: "This is an article or game info card. Blah blah blah" 
+      },
+      {
+        title: "Title 4",
+        subheader: "Subhead 4",
+        body: "This is an article or game info card. Blah blah blah" 
+      },
+      {
+        title: "Title 5",
+        subheader: "Subhead 5",
+        body: "This is an article or game info card. Blah blah blah" 
+      },
+      {
+        title: "Title 6",
+        subheader: "Subhead 6",
+        body: "This is an article or game info card. Blah blah blah" 
+      }
+    ]
+  }
+
   return (
-    <KeyboardAvoidingView>
-          <Header/>
-      <View>
+    <View style={styles.container}>
+      <KeyboardAvoidingView>
+        <Header/>
+      </KeyboardAvoidingView>
+      
 
-        <View>
-
-        </View>
-        <View>
-
-        </View>
-      </View>
-    </KeyboardAvoidingView>
+      <ScrollView>
+        {this.state.articles.map(article => (
+        
+        <Card
+          cardhead={article.title}
+          cardsubhead={article.subheader}
+          cardbody={article.body}
+        />
+        ))}
+      </ScrollView>
+    </View>
   );
 }
+
+const styles  = StyleSheet.create({
+  container: {
+    backgroundColor: `#363534`,
+    height: `100%`,
+    width: `100%`
+  },
+  containerText: {
+    color: `skyblue`
+  }
+})
