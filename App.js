730c5109b50e09113aa04d757f9692ccbaf07ac5
Robert Butler <robwbutler@gmail.com>
2019-07-11 23:13:52 -0400

    more styling, gamecard component added

--- App.js
+++ App.js
@@ -1,46 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, ScrollView, View, KeyboardAvoidingView } from 'react-native';
 import Header from './components/header/header';
-import Card from './components/card/card';
+import ArticleCard from './components/articlecard/articleCard';
 import Footer from './components/footer/footer';
+import GameCard from './components/gamecard/gameCard';
 
 
 export default function App() {
 
-  //these state values were hardcoded for testing, delete and replace with fetch requests, as well as a property for the thumbnail/article image
-  state = {
-    articles: [
-      {
-        title: "Title 1",
-        subheader: "Subhead 1",
-        body: "This is an article or game info card. Blah blah blah" 
-      },
-      {
-        title: "Title 2",
-        subheader: "Subhead 2",
-        body: "This is an article or game info card. Blah blah blah" 
-      },
-      {
-        title: "Title 3",
-        subheader: "Subhead 3",
-        body: "This is an article or game info card. Blah blah blah" 
-      },
-      {
-        title: "Title 4",
-        subheader: "Subhead 4",
-        body: "This is an article or game info card. Blah blah blah" 
-      },
-      {
-        title: "Title 5",
-        subheader: "Subhead 5",
-        body: "This is an article or game info card. Blah blah blah" 
-      },
-      {
-        title: "Title 6",
-        subheader: "Subhead 6",
-        body: "This is an article or game info card. Blah blah blah" 
-      }
-    ]
+      /*   These state values were hardcoded for testing; delete them replace with working fetch requests
+      */  
+    state = {
+      articles: [
+        {
+          title: "Title 1",
+          subheader: "Subhead 1",
+          body: "This is an article card. Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah" 
+        },
+        {
+          title: "Title 2",
+          subheader: "Subhead 2",
+          body: "This is an article card. Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah" 
+        },
+        {
+          title: "Title 3",
+          subheader: "Subhead 3",
+          body: "This is an article card. Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah" 
+        },
+        {
+          title: "Title 4",
+          subheader: "Subhead 4",
+          body: "This is an article card. Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah" 
+        },
+        {
+          title: "Title 5",
+          subheader: "Subhead 5",
+          body: "This is an article card. Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah" 
+        },
+        {
+          title: "Title 6",
+          subheader: "Subhead 6",
+          body: "This is an article card. Blah blah blah Blah blah blah Blah blah blah Blah blah blah Blah blah blah" 
+        }
+      ]
   }
 
   return (
@@ -53,12 +55,21 @@ export default function App() {
       <ScrollView>
         {this.state.articles.map(article => (
         
-        <Card
+        <ArticleCard
           cardhead={article.title}
           cardsubhead={article.subheader}
           cardbody={article.body}
         />
         ))}
+
+        {/* The GameCard below shouldn't be on the homepage, obviously, it's just here to see styling and components until we get routing and pages set up */}
+
+        <GameCard
+          title="Apex Legends"
+          platforms="PC, PS4, XBO"
+          releasedate="2019"
+          description="This battle royale from Respawn Entertainment is focused on teams and special abilities"
+        />
       </ScrollView>
       <Footer/>
     </View>
