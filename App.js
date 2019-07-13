a18f14876a906abea311ab6078afe1dd0b2e80b6
Derrick <dderrickmatheww@gmail.com>
2019-07-13 10:08:43 -0400

    update on search bar

--- App.js
+++ App.js
@@ -2,19 +2,108 @@ import React from 'react';
 import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
 import Header from './components/header/header';
 import SearchBar from './components/SearchBar/SearchBar'
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 export default function App() {
-  return (
-    <View style={{flex: 1, backgroundColor: "grey"}}>
+  
+  class Home extends React.Component {
+            
+    getSearchResults = (text) => {
+      // this.setState({onCall: true});
+      
+      let searchQuery = text;
+      let cacheName = text.toLowerCase();
+      searchQuery = searchQuery.toLowerCase().replace(' ', '%');
 
-       <Header />
+      this.props.navigation.navigate('Details', {
+        text: text
+      });
+      
+  }
+    render() {
+      return (
+        <View style={{flex: 1, backgroundColor: "grey"}}>
 
-      <KeyboardAvoidingView>
+          <Header />
 
-          <SearchBar />
+          <KeyboardAvoidingView>
 
-      </KeyboardAvoidingView>
+              <SearchBar getSearchResults={this.getSearchResults}  />
 
-    </View>
+          </KeyboardAvoidingView>
+
+        </View>
+      
+      )
+    }
+  }
+  class Card extends React.Component {
+    state = {
+      searchResults: []
+    }
+    componentDidMount() {
+
+      const { navigation } = this.props;
+      const text = navigation.getParam('text', '');
+      let searchQuery = text;
+      let cacheName = text.toLowerCase();
+      searchQuery = searchQuery.toLowerCase().replace(' ', '%');
+      let url = 'https://www.giantbomb.com/api/search/?format=json&api_key=99ec1d8980f419c59250e12a72f3b31d084e9bf9&query=' + searchQuery + '&resources=game&limit=1'
+            
+            fetch(url)
+            .then(function(response) {
+              
+                response.json().then( async data => {
+
+                    let value = await AsyncStorage.getItem(cacheName);
+
+                    if ( value ) {
+                        value = JSON.parse(value);
+                    } 
+                    else {
+                        await AsyncStorage.setItem(cacheName, JSON.stringify(data.results));
+                        value = data.results;
+                    }
+                    this.setState({searchResults: value});
+                })
+            })
+            .catch(function() {
+                if (err) {
+
+                }
+            });
+
+      }
+  }
+  const AppNavigator = createStackNavigator(
+    {
+      Home: Home,
+      Card: Card
+    },
+    {
+      initialRouteName: "Home"
+    }
   );
+
+  module.exports = createAppContainer(AppNavigator);
+  
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: "grey"}}>
+
+        <Header />
+
+        <KeyboardAvoidingView>
+
+            
+
+        </KeyboardAvoidingView>
+
+      </View>
+    
+    )
+  }
+ 
 }
+
+
--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,56 +1,16 @@
 import React, { Component } from 'react';
 import { View, TextInput, Button, AsyncStorage } from 'react-native';
 
+
 export default class SearchBar extends Component {
     
         state = {
             onCall: true,
-            text: ''
+            text: '',
+            page: ''
         }
-        
 
-        getSearchResults = () => {
-            this.setState({onCall: true});
-            let searchQuery = this.state.text;
-            let cacheName = this.state.text.toLowerCase();
-            searchQuery = searchQuery.toLowerCase().replace(' ', '%');
-            let url = 'https://www.giantbomb.com/api/search/?format=json&api_key=99ec1d8980f419c59250e12a72f3b31d084e9bf9&query=' + searchQuery + '&resources=game&limit=1'
-            
-            fetch(url)
-            .then(function(response) {
-               
-                response.json().then(async data => {
-
-                    let value = await AsyncStorage.getItem(cacheName);
-
-                    if ( value ) {
-
-                        console.log(value);
-                        value = JSON.parse(value);
-
-                    } 
-                    else {
-
-                        await AsyncStorage.setItem(cacheName, JSON.stringify(data.results))
-                        value = data.results;
-                        console.log('its running');
-
-                    }
-                    
-                })
-
-            })
-            .catch(function() {
-                if (err) {
-
-                }
-            });
-        }
       
-        getCard = () => {
-            console.log('hello')
-        }
-
         render() {
           return (
               <View>
@@ -59,10 +19,9 @@ export default class SearchBar extends Component {
                 onChangeText={(text) => this.setState({text})}
                 value={this.state.text}
                 />
-                <Button title='Search' onPress={this.getSearchResults() && this.getCard()}/>
+                <Button title='Search' onPress={() => this.props.getSearchResults(this.state.text)}/>
             </View>
           );
         }
       
 }
-
