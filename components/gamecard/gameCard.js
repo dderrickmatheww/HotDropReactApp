268556b6f8495e9105915575f82a4811148eb5bb
Robert Butler <robwbutler@gmail.com>
2019-08-15 22:57:10 -0400

    comment modal styling and box art modal

--- components/gamecard/gameCard.js
+++ components/gamecard/gameCard.js
@@ -1,25 +1,65 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native';
 
 export default class GameCard extends React.Component {
-        render() {
-            return(
+    state = {
+        isVisible: false
+    }
+
+    closeModalFunc = () => {
+        this.setState({isVisible: false});
+    }
+
+    render() {
+        return(
+            <View>
                 <View style={styles.card}>
-                    <View style={styles.thumbcontainer}>
+                    <TouchableOpacity 
+                        style={styles.thumbcontainer}
+                        onPress={() => this.setState({isVisible: true})}
+                    >
                         <Image
                             style={styles.gamelogo}
                             source={{ uri: this.props.picture }}
                         />
-                    </View>
+                    </TouchableOpacity>
                     <View style={styles.cardtext}>
                         <Text style={styles.title}>{this.props.title}</Text>
                         <Text style={styles.info}>Platforms: {this.props.platforms}</Text>
                         <Text style={styles.info}>Release Date: {this.props.releasedate}</Text>
                         <Text style={styles.description}>{this.props.description}</Text>
                     </View>
-                </View>  
-            )
-        }
+                </View> 
+
+                <View style={styles.container}>
+                    <Modal
+                        animationType='slide'
+                        onRequestClose={() => console.log('no warning')}
+                        transparent={true}
+                        visible={this.state.isVisible}
+                    >
+                        <View style={{backgroundColor: 'rgba(96, 96, 128, 0.66)', height: '100%'}}>
+                            <View style={styles.modalView} >
+                                <TouchableOpacity
+                                    onPress={this.closeModalFunc}
+                                >
+                                    <Text style={styles.closeText}>X</Text>
+                                </TouchableOpacity>
+                                <View style={styles.modalContent}>
+                                <Image 
+                                    style={styles.modalImage}
+                                    source={{ uri: this.props.modal}}
+                                    resizeMode='contain'
+                                />
+                                
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
+                </View>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -92,6 +132,34 @@ const styles = StyleSheet.create({
     },
     cardtext: {
         flex: 1
+    },
+    modalImage: {
+        bacgkroundColor: `darkslategray`,
+        height: 500,
+        width: 300
+    },
+    modalView: {
+        backgroundColor: `rgb(1, 0, 32)`,
+        margin: 40,
+        padding: 10,
+        borderRadius: 1,
+        borderWidth: 1,
+        borderColor: 'rgb(206, 212, 218)',
+        elevation: 20
+    },
+    closeText: {
+        color: 'rgb(135, 206, 250)',
+        backgroundColor: 'rgb(1, 0, 128)',
+        borderRadius: 1,
+        width: 32,
+        padding: 6,
+        alignSelf: 'flex-end',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: 'darkslategray',
+    },
+    modalContent: {
+        justifyContent: 'center',
+        alignItems: 'center',
     }
-
 })
